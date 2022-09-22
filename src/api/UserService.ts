781dd1b8f00924d47a85a045ff59581c042a68c7
cnxk <cos4338@gmail.com>
2022-09-23 01:50:47 +0900

    Feat: create api module to call user datas

---
+++ src/api/UserService.ts
@@ -0,0 +1,22 @@
+import { Params, User } from 'src/types/types';
+import instance from './axios';
+
+export const getUserService = async (params?: Params) => {
+  const response = await instance.get('/users', { params: params });
+  return response;
+};
+
+export const postUserService = async (user: User) => {
+  const response = await instance.post('/users', user);
+  return response;
+};
+
+export const putUserService = async (user: User) => {
+  const response = await instance.put('/users', user);
+  return response;
+};
+
+export const deleteUserService = async (id: string) => {
+  const response = await instance.delete('/users');
+  return response;
+};
