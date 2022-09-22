3d75d5b01983c2d4899ff06ddfcb39dc9c635274
Dev-jwJeong <rgn95652@gmail.com>
2022-09-23 00:37:39 +0900

    Feat : 사용자 정보 store 저장

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -2,6 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   user: null,
+  userTotal: null,
 };
 
 export const userSlice = createSlice({
@@ -11,9 +12,12 @@ export const userSlice = createSlice({
     setUser: (state, action) => {
       state.user = action.payload;
     },
+    setUserTotal: (state, action) => {
+      state.userTotal = action.payload;
+    },
   },
 });
 
-export const { setUser } = userSlice.actions;
+export const { setUser, setUserTotal } = userSlice.actions;
 
 export default userSlice.reducer;
