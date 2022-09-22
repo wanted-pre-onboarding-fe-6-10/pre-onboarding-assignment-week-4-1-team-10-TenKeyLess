452eeed554ab0c25595f7b762ee928f367649b51
cnxk <cos4338@gmail.com>
2022-09-22 22:50:27 +0900

    Feat: create  get account list module

--- src/api/AccountService.ts
+++ src/api/AccountService.ts
@@ -1,6 +1,13 @@
 import instance from './axios';
 
-export const getAccountService = async () => {
-  const response = await instance.get('/accounts');
+export const getAccountService = async (
+  page?: string,
+  limit?: string,
+  sort?: string | null,
+  order?: 'desc' | 'asc' | null
+) => {
+  const response = await instance.get(
+    `/accounts?_page=${page}&_limit=${limit}&_sort=${sort}&_order=${order}`
+  );
   return response;
 };
--- src/store/services/accountSlice.ts
+++ src/store/services/accountSlice.ts
@@ -2,14 +2,17 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { getAccountService } from '../../api/AccountService';
 import { Account } from 'src/types/types';
 import { AxiosError } from 'axios';
+import { RemoveToken } from '../../repository/TokenRepository';
+import { InitialParams } from './pageSlice';
 
-const initialState: Account[] | null = [];
+const initialState: Account[] | 'jwt expired' | null = [];
 
 export const getAccountList = createAsyncThunk(
   'accounts/getList',
-  async (_, { rejectWithValue }) => {
+  async (params: InitialParams, { rejectWithValue }) => {
+    const { page, limit, sort, order } = params;
     try {
-      const { data } = await getAccountService();
+      const { data } = await getAccountService(page, limit, sort, order);
       return data;
     } catch (e) {
       if (e instanceof AxiosError) {
@@ -22,13 +25,24 @@ export const getAccountList = createAsyncThunk(
 
 const accountSlice = createSlice({
   name: 'accounts',
-  initialState: { isLoading: false, list: initialState },
-  reducers: {},
+  initialState: { isLoading: false, list: initialState, pageList: [''] },
+  reducers: {
+    getPageList: (state, action) => {
+      const pages = Array.from(
+        { length: Math.ceil(state.list.length / action.payload) },
+        (value, index) => (index + 1).toString()
+      );
+      state.pageList = pages;
+    },
+  },
   extraReducers: builder => {
     builder.addCase(getAccountList.pending, (state, action) => {
       return { ...state, isLoading: true };
     });
     builder.addCase(getAccountList.fulfilled, (state, action) => {
+      if (action.payload === 'jwt expired') {
+        RemoveToken();
+      }
       return { ...state, list: action.payload, isLoading: false };
     });
     builder.addCase(getAccountList.rejected, (state, action) => {
@@ -38,4 +52,5 @@ const accountSlice = createSlice({
   },
 });
 
+export const { getPageList } = accountSlice.actions;
 export default accountSlice.reducer;
