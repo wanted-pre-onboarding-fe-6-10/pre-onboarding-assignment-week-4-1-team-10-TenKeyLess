679c1dc441670df995280a0da84c53ed04eb9565
cnxk <cos4338@gmail.com>
2022-09-22 09:07:34 +0900

    Feat: Login 기능 구현

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,16 +1,6 @@
+import LoginBox from './component/LoginBox';
+
 const Login = () => {
-  return (
-    <form>
-      <label>
-        email
-        <input type="email" />
-      </label>
-      <label>
-        password
-        <input type="password" />
-      </label>
-      <button>로그인</button>
-    </form>
-  );
+  return <LoginBox />;
 };
 export default Login;
---
+++ src/pages/Login/component/LoginInput.tsx
@@ -0,0 +1,47 @@
+import authService from '../../../api/AuthService';
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { ReducerType } from 'store/rootReducer';
+import { inputChage } from '../../../store/services/authSlice';
+import { SetToken } from '../../../repository/TokenRepository';
+import { useNavigate } from 'react-router-dom';
+
+const LoginInput = () => {
+  const nav = useNavigate();
+  const input = useSelector((state: ReducerType) => state.auth.input);
+  const dispatch = useDispatch();
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    dispatch(inputChage({ ...input, [name]: value }));
+  };
+
+  const handleLogin: React.MouseEventHandler<HTMLButtonElement> = async e => {
+    try {
+      e.preventDefault();
+      const { data } = await authService(input);
+      const token = data.accessToken;
+      SetToken(token);
+      nav('/account');
+    } catch {
+      console.error('error');
+    }
+  };
+
+  return (
+    <form>
+      <label>
+        email
+        <input type="email" name="email" onChange={handleInputChange} />
+      </label>
+      <label>
+        password
+        <input type="password" name="password" onChange={handleInputChange} />
+      </label>
+      <button onClick={handleLogin}>로그인</button>
+    </form>
+  );
+};
+
+export default LoginInput;
