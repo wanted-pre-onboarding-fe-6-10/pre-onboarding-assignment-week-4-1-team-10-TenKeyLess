a55453a39be8d91c06c85252e14e8095eda577df
Dev-jwJeong <rgn95652@gmail.com>
2022-09-22 17:51:30 +0900

    Chore : 프로젝트 초기 세팅

--- craco.config.js
+++ craco.config.js
@@ -1,24 +1,4 @@
-const CracoAlias = require('craco-alias');
-
 module.exports = {
-  plugins: [
-    {
-      plugin: CracoAlias,
-      options: {
-        source: 'options',
-        baseUrl: './',
-        aliases: {
-          '@api': './src/api',
-          '@assets': './src/assets',
-          '@components': './src/components',
-          '@hooks': './src/hooks',
-          '@pages': './src/pages',
-          '@style': './src/style',
-          '@utils': './src/utils',
-        },
-      },
-    },
-  ],
   style: {
     postOptions: {
       plugins: [require('tailwindcss'), require('autoprefixer')],
