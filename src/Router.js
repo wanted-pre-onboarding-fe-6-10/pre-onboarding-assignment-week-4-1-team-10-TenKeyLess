027a708650856175fe97d5c9461bc6ca339ae7f1
Dev-jwJeong <rgn95652@gmail.com>
2022-09-23 00:36:42 +0900

    Feat : 사용자 정보 router 추가

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AccountContent from './page/AccountContent/AccountContent';
 import Login from './page/Login/Login';
+import UserContent from './page/UserContent/UserContent';
 
 const Router = () => {
   return (
@@ -9,6 +10,7 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/account_list" element={<AccountContent />} />
+        <Route path="/users_list" element={<UserContent />} />
       </Routes>
     </BrowserRouter>
   );
