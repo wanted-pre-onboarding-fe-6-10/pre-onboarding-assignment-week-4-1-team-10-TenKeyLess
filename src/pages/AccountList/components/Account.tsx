f049bee6f1cbb8edbcf592fa30c826998e680922
cnxk <cos4338@gmail.com>
2022-09-22 16:18:50 +0900

    Feat: create AccountList page components

---
+++ src/pages/AccountList/components/Account.tsx
@@ -0,0 +1,5 @@
+const Account = () => {
+  return <div>list</div>;
+};
+
+export default Account;
---
+++ src/pages/AccountList/components/AccountBox.tsx
@@ -0,0 +1,13 @@
+import Account from './Account';
+import AccountTitle from './AccountTitle';
+
+const AccountBox = () => {
+  return (
+    <>
+      <AccountTitle />
+      <Account />
+    </>
+  );
+};
+
+export default AccountBox;
