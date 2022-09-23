8741f25424627218af1c2701a2f4ccea1223a50f
Dev-jwJeong <rgn95652@gmail.com>
2022-09-24 08:46:39 +0900

    Feat : 페이지네이션 props 추가

--- src/api/userAPI.js
+++ src/api/userAPI.js
@@ -1,6 +1,14 @@
 import { instance } from './customAxios';
 
-export const user = async () => {
+export const userPage = async page => {
+  try {
+    const response = await instance.get(`/users?_page=${page}&_limit=20`);
+    return response.data;
+  } catch (e) {
+    throw e;
+  }
+};
+export const userTotalList = async () => {
   try {
     const response = await instance.get('/users');
     return response.data;
