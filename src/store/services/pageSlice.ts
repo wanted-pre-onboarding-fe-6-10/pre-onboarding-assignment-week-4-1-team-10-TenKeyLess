823898b51b2bc74f466a34ca3b36978724f4ab1d
cnxk <cos4338@gmail.com>
2022-09-23 09:54:57 +0900

    Feat: Add user page state

--- src/store/services/pageSlice.ts
+++ src/store/services/pageSlice.ts
@@ -1,4 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { getUserService } from '../../api/UserService';
 import { Params } from 'src/types/types';
 import { getAccountService } from '../../api/AccountService';
 
@@ -6,6 +7,17 @@ export const getFullAccountList = createAsyncThunk(
   'page/accounts',
   async (limit: number, { rejectWithValue }) => {
     const { data } = await getAccountService();
+    const pages = Array.from({ length: Math.ceil(data.length / limit) }, (v, i) =>
+      (i + 1).toString()
+    );
+    return pages;
+  }
+);
+
+export const getFullUserList = createAsyncThunk(
+  'page/users',
+  async (limit: number, { rejectWithValue }) => {
+    const { data } = await getUserService();
     const pages = Array.from({ length: Math.ceil(data.length / limit) }, (v, i) =>
       (i + 1).toString()
     );
@@ -23,7 +35,7 @@ const initialParams: Params = {
 
 const pageSlice = createSlice({
   name: 'page',
-  initialState: { params: initialParams, accountPages: [''] },
+  initialState: { params: initialParams, accountPages: [''], userPages: [''] },
   reducers: {
     updatePage: (state, action) => {
       state.params = { ...state.params, _page: action.payload };
@@ -37,13 +49,16 @@ const pageSlice = createSlice({
     updateOrder: (state, action) => {
       state.params = { ...state.params, _order: action.payload };
     },
-    resetParams: (state, action) => {
+    resetParams: state => {
       state.params = initialParams;
     },
   },
   extraReducers: builder => {
     builder.addCase(getFullAccountList.fulfilled, (state, action) => {
       state.accountPages = action.payload;
+    });
+    builder.addCase(getFullUserList.fulfilled, (state, action) => {
+      state.userPages = action.payload;
     });
   },
 });
