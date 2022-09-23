a4a6038ae48e96690b410af595b886ba70866366
cnxk <cos4338@gmail.com>
2022-09-23 21:58:49 +0900

    Chore: setting tailwindcss and craco

--- craco.config.js
+++ craco.config.js
@@ -1,13 +1,9 @@
 const CracoAlias = require('craco-alias');
 
 module.exports = {
-  plugins: [
-    {
-      plugin: CracoAlias,
-      options: {
-        source: 'tsconfig',
-        tsConfigPath: 'tsconfig.paths.json',
-      },
+  style: {
+    postcssOptions: {
+      plugins: [require('tailwindcss'), require('autoprefixer')],
     },
-  ],
+  },
 };
