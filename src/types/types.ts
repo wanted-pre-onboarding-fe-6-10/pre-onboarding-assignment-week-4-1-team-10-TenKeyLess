18603fffcbdf15c6770ef65e9644e00a60bbe025
cnxk <cos4338@gmail.com>
2022-09-25 09:53:19 +0900

    Feat: Create useSetting type

--- src/types/types.ts
+++ src/types/types.ts
@@ -20,8 +20,8 @@ export interface Account {
 }
 
 export interface Params {
-  _page: string;
-  _limit: '10' | '20' | '30';
+  _page: string | null;
+  _limit: '10' | '20' | '30' | null;
   _sort: string | null;
   _order: 'desc' | 'asc' | null;
   broker_id: string | null;
@@ -30,6 +30,7 @@ export interface Params {
   q: string | null;
   _expand: 'user' | null;
   _embed: 'accounts' | null;
+  allow_marketing_push: boolean | null;
 }
 
 export interface User {
@@ -48,6 +49,7 @@ export interface User {
   created_at: string;
   updated_at: string;
   accounts: Account[];
+  setting?: UserSetting | null;
 }
 
 export interface UserSetting {
@@ -59,4 +61,32 @@ export interface UserSetting {
   is_staff: boolean;
   created_at: string;
   updated_at: string;
+}
+
+export interface Brokers {
+  '209': '유안타증권';
+  '218': '현대증권';
+  '230': '미래에셋증권';
+  '238': '대우증권';
+  '240': '삼성증권';
+  '243': '한국투자증권';
+  '247': '우리투자증권';
+  '261': '교보증권';
+  '262': '하이투자증권';
+  '263': 'HMC투자증권';
+  '264': '키움증권';
+  '265': '이베스트투자증권';
+  '266': 'SK증권';
+  '267': '대신증권';
+  '268': '아이엠투자증권';
+  '269': '한화투자증권';
+  '270': '하나대투자증권';
+  '279': '동부증권';
+  '280': '유진투자증권';
+  '288': '카카오페이증권';
+  '287': '메리츠종합금융증권';
+  '290': '부국증권';
+  '291': '신영증권';
+  '292': 'LIG투자증권';
+  '271': '토스증권';
 }
